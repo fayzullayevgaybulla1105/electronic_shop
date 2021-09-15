4ec7c89dcb92d14de2611ca33e778a91af09afbf
Gaybulla <fayzullayevgn@gmail.com>
2021-09-15 13:46:43 +0500

    allow access

--- src/server.js
+++ src/server.js
@@ -16,6 +16,12 @@ const server = express()
 
 let router = express.Router()
 //third party gloabl middlewares
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 server.use(express.urlencoded({ extended: true }))
 server.use(express.json())
 // server.use(express.json())
