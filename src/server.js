02197f262cc9811769faa376722d06b526c40bf3
Gaybulla <fayzullayevgn@gmail.com>
2021-09-15 13:48:59 +0500

    tes

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const server = express()
 let router = express.Router()
 //third party gloabl middlewares
 
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
