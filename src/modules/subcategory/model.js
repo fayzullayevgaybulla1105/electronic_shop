60c93e0e88b47594225e8ac3fea75a0ee243415b
Gaybulla <fayzullayevgn@gmail.com>
2021-09-15 14:19:30 +0500

    subcategories Completed

--- src/modules/subcategory/model.js
+++ src/modules/subcategory/model.js
@@ -4,20 +4,26 @@ import path from 'path'
 
 let categor = fs.readFileSync(path.join(process.cwd(), 'src', 'database', 'subcategory.json'), 'utf-8')
 categor = categor ? JSON.parse(categor) : []
-console.log(categor);   
-const all = () => {
+// console.log(categor);  
+const all = () => {      
 
     let subcatName = []
     for (let i in categor) {
-
-        for (let j in categor[i].subCategories) {
+  
+        // cons  ole.log(categor[i]);  
+        // console.log(categor[i].subCategories);  
+        // for (let j in categor[i].subCategories) {
             subcatName.push({
-                subCategoryId: categor[i].subCategories[j].subCategoryId,
-                subCategoryName: categor[i].subCategories[j].subCategoryName
+                subCategoryId: categor[i].subCategory_id,
+                subCategoryName: categor[i].subCategory_name,
+                categoryId: categor[i].category_id
             })
-        }
+
+        // }  
     }
+        console.log(subcatName);
     return subcatName
+
 }
 
 
